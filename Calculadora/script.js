bfc840031a19ce0c00355abc4517f7653ce88e33
Otávio <83560427+otaviocsr@users.noreply.github.com>
2023-05-11 21:35:35 -0300

    Add files via upload

---
+++ Calculadora/script.js
@@ -0,0 +1,9 @@
+function addToDisplay(value) {
+  document.getElementById('display').value += value;
+}
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+function calculate() {
+  document.getElementById('display').value = eval(document.getElementById('display').value);
+}
